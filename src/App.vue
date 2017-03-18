e8ae755e2728063c3841596122ca6ee9c9004103
ellebayless <elle@ellebayles.com>
2017-03-18 12:27:27 -0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="app">
+
+    <input type="text" v-model="todo">
+    <button type="button" @click="addTodo">Add To List!</button>
+    <h2>Todo List</h2>
+    <ul>
+        <li v-for="todo in todoList">
+            {{todo}}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App',
+            todo: '',
+            todoList: []
+        }
+    },
+    methods: {
+        addTodo: function() {
+            this.todoList.push(this.todo)
+        }
+    }
+}
+</script>
+
+<style lang="scss"></style>
