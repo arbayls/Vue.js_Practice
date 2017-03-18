baea56d1fe91f0ac056d4a30ae07bee8556dde6e
ellebayless <elle@ellebayles.com>
2017-03-18 13:31:16 -0700

    stretch goals complete

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,15 @@
     <ul>
         <li v-for="todo in todoList">
             {{todo}}
+            <button type="button" @click="completeTodo(todo)">Complete</button>
+            <button type="button" @click="removeTodo(todo)">Remove</button>
         </li>
+    </ul>
+    <h2>Completed Todos</h2>
+    <ul>
+      <li v-for="todo in completedTodos">
+        {{todo}}
+      </li>
     </ul>
 </div>
 </template>
@@ -19,12 +27,21 @@ export default {
         return {
             msg: 'Welcome to Your Vue.js App',
             todo: '',
-            todoList: []
+            todoList: [],
+            completedTodos: []
         }
     },
     methods: {
         addTodo: function() {
             this.todoList.push(this.todo)
+            this.todo = ''
+        },
+        completeTodo: function(listicle) {
+            this.completedTodos.push(listicle)
+            this.todoList.splice(this.todoList.indexOf(listicle), 1)
+        },
+        removeTodo: function(listicle) {
+            this.todoList.splice(this.todoList.indexOf(listicle), 1)
         }
     }
 }
